086dc907c99edfaede069dfd47e45d4ab03c1168
NatapolLim <natapolllim@gmail.com>
2023-02-10 16:46:10 +0700

    delete apps/test-project.py

--- apps/test-project.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def app():
-    t_choice = st.sidebar.radio("T-Test Settings",["One Sample Data","One Sample Stats","Paired Sample Data","Two Sample Data","Two Sample Stats"])
-    if t_choice == "Profile":
-        pass
